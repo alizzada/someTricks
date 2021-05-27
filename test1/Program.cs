f7d941bdb97851925f38ab7bc9c03dd045d475dd
Ali Alizada <a.alizada@tayqatech.com>
2021-05-27 17:04:35 +0400

    Adding new console project

---
+++ test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {3,2,1,4,5};
+
+            int middleNumber = 0;
+
+            if ((array.Length) % 2 == 0)
+            {
+                middleNumber = array[array.Length / 2 - 1];
+            }
+            else
+                middleNumber = array[array.Length / 2];
+
+
+
+             Console.WriteLine(middleNumber);
+            Console.ReadKey();
+        }
+    }
+}
